6992189d6f8a6ce6b7b90ff83db2e306c17bc989
Fernando Marca Magalhães <fernandomarca@hotmail.com>
2021-09-25 23:19:27 -0300

    single-spa

---
+++ navbar/src/navbar.tsx
@@ -0,0 +1,72 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Box, Typography, Tabs, Tab } from "@material-ui/core";
+import * as singleSpa from "single-spa";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        singleSpa.navigateToUrl("/");
+        break;
+      case 1:
+        singleSpa.navigateToUrl("/appl");
+        break;
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Home" {...a11yProps(0)} />
+          <Tab label="Appl-react" {...a11yProps(1)} />
+          <Tab label="App2" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}></TabPanel>
+      <TabPanel value={value} index={1}></TabPanel>
+      <TabPanel value={value} index={2}></TabPanel>
+    </Box>
+  );
+}
